cdeefff3733c78357af831766ed5fa6157293614
hackboxlive <singhsarvshakti127@gmail.com>
2018-04-21 23:34:07 +0530

    Adding command_line.cpp

--- include/command_line.hpp
+++ include/command_line.hpp
@@ -19,7 +19,14 @@ class command_line	{
 
 	private:
 		state_ state;
-		
+		network network_obj;
+		std::vector<std::wstring> interfaces;
+		std::map<std::string, std::set<address_type> > aps;
+		std::vector<std::string> targets;
+
+		void show_menu();
+		void show_header();
+		void show_action();
 }
 
 #endif
