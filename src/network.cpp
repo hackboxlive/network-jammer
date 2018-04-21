badd9415b5ecb680f85abce6ea7fac33dbb6cace
hackboxlive <singhsarvshakti127@gmail.com>
2018-04-21 16:10:36 +0530

    added get_connected_devices() and get_access_points()

--- src/network.cpp
+++ src/network.cpp
@@ -30,3 +30,36 @@ std::vector<std::wstring> network::get_interfaces()	{
 
 	return interface_names;
 }
+
+std::vector<std::string> network::get_connected_devices()	{
+	std::vector<std::string> targets;
+	NetworkInterface iface = NetworkInterface(iface_name);
+	NetworkInterface::Info info_scanner = iface.info();
+
+	for(const auto &target : network_range)	{
+		EthernetII scan = ARP::make_arp_request(target, info_scanner.ip_addr, info_scanner.hw_addr);
+		std::unique_ptr<PDU> reply(sender.send_recv(scan, iface));
+		if(reply)	{
+			targets.push_back((reply->rfind_pdu<ARP>()).sender_hw_addr().to_string());
+			std::cout << "Target found : [" << target << " / " << targets.back() << " ]" << std::endl;
+		}
+	}
+
+	return targets;
+}
+
+std::map<std::string, std::set<address_type> > network::get_access_points()	{
+	SnifferConfiguration config;
+	config.set_promisc_mode(true);
+	config.set_filter("type mgt subtype beacon");
+	config.set_rfmon(true);
+
+	Sniffer sniffer(iface_name, config);
+	std::thread scanThread(&Network::stopScan, &scanning);
+	scanThread.detach();
+
+	sniffer.sniff_loop(make_sniffer_handler(this, &Network::scanCallback));
+
+	return accessPoints;
+}
+
