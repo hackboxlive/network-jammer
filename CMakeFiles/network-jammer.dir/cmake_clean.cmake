e8cafb4c5ba6a926cc08315f084230aa0a9d6f20
hackboxlive <singhsarvshakti127@gmail.com>
2018-05-01 04:31:34 +0530

    cmake done, runtime problem now

---
+++ CMakeFiles/network-jammer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/network-jammer.dir/main.cpp.o"
+  "CMakeFiles/network-jammer.dir/command_line.cpp.o"
+  "CMakeFiles/network-jammer.dir/network.cpp.o"
+  "network-jammer.pdb"
+  "network-jammer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/network-jammer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
